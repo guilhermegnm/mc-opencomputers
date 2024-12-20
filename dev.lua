7c10875202b01fcd9b9bafd5475245e18ae82cea
Guilherme <guilherme.gn.monteiro@gmail.com>
2024-12-20 18:26:06 +0100

    fast commit

--- dev.lua
+++ dev.lua
@@ -1,1 +1,13 @@
+component = require("component")
 ae2 = component.proxy("3758bac3-4b04-4778-9fc4-05b563642f4f")
+
+cpus = ae2.getCpus()
+
+busy = 0
+
+for i in ipairs(cpus) do
+    if cpus[i].busy = true then
+        busy += 1
+        print(busy)
+    end
+end
